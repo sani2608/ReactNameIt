40e783148c21504c36124414c400065bf4755b66
sani2608 <sanikumarsahani@gmail.com>
2022-02-20 23:06:00 +0530

    converted class component to function component added debounce for input

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,49 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import Header from "../Header/Header";
 import ResultsContainer from "../results/ResultsContainer";
 import SearchBox from "../SearchBox/SearchBox";
 import './App.css';
 
 const name = require('@rstacruz/startup-name-generator');
-class App extends React.Component{
+
+
+const App = () => {
+    const [headerText] = useState("Name It");
+    const [headerExpanded, setHeaderExpanded] = useState(true);
+    const [suggestedNames, setSuggestedNames] = useState([]);
+
+    const handleInputChange = (inputText) => {
+        console.log(inputText);
+        setHeaderExpanded(!inputText);
+        setTimeout(() => {
+            setSuggestedNames(inputText ? name(inputText) : []);
+        }, 2000);
+    }
+    const debounce = (func) => {
+        let timer = null;
+        return function (...args) {
+            const context = this;
+            if (timer) clearTimeout(timer);
+            timer = setTimeout(() => {
+                timer = null;
+                func.apply(context, args);
+            }, 500);
+        }
+    }
+    const optimizedFn = useCallback(debounce(handleInputChange), []);
+    return (
+        <div>
+            <Header
+                headTitle={headerText}
+                headerExpanded={headerExpanded}
+            />
+            <SearchBox onInputChange={optimizedFn} />
+            <ResultsContainer suggestedNames={suggestedNames} />
+        </div>
+    )
+}
+
+/** class App extends React.Component{
     state = {
         headerText: "Name It!",
         headerExpanded: true,
@@ -28,4 +66,5 @@ class App extends React.Component{
         )
     }
 }
+*/
 export default App;
